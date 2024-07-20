4fcb6c5d0f403ed110c527b1f39a3806983b230c
Artezi0 <193.rayyan@mpuin-jkt.sch.id>
2024-07-20 09:29:39 +0700

    minor updates

---
+++ src/app/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAH2EpU0xyDblLKysSFVMGBDuITlLwpevM",
+  authDomain: "notoruang-ad8c8.firebaseapp.com",
+  projectId: "notoruang-ad8c8",
+  storageBucket: "notoruang-ad8c8.appspot.com",
+  messagingSenderId: "470575970150",
+  appId: "1:470575970150:web:c6c60fb6231e391a8c7aa7"
+};
+
+const app = initializeApp(firebaseConfig);
